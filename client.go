9e5f867982d4c9bcca2dfeda6f4ff9899f052fdc
Oleg Borodin <onborodin@gmail.com>
2023-03-05 09:36:37 -0500

    added helm

--- client.go
+++ client.go
@@ -1,28 +1,67 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"os"
+
+	"google.golang.org/grpc"
+
+	pbHello "pmapp/api/hello"
 )
 
-const (
-	port = 8080
-	host = "server"
-)
+var host string = "server"
+
+const port uint32 = 9001
+
+func install() error {
+	var err error
+
+	if len(os.Args) > 1 {
+		host = os.Args[1]
+	}
+	address := fmt.Sprintf("%s:%d", host, port)
+	log.Println("start client for", address)
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Println("error dial:", err)
+		return err
+	}
+	defer conn.Close()
+
+	req := pbHello.InstallRequest{
+		Hostname: "localhost",
+		Port:     12345,
+	}
+
+	pbClient := pbHello.NewHelloClient(conn)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	stream, err := pbClient.Install(ctx, &req)
+	if err != nil {
+		return err
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		if res.Done {
+			log.Println("installation is finished!")
+			break
+		}
+		log.Println("installation is incomplete")
+	}
+	return err
+}
 
 func main() {
 	log.Println("start client")
-
-	for {
-		uri := fmt.Sprintf("http://%s:%s", host, port)
-		resp, err := http.Get(uri)
-		if err != nil {
-			log.Println("request error:", err)
-		}
-		resp.Body.Close()
-
-		log.Println("response status:", resp.Status)
-		time.Sleep(1 * time.Second)
+	err := install()
+	if err != nil {
+		fmt.Println("client error:", err)
 	}
 }
--- server.go
+++ server.go
@@ -1,18 +1,100 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	pbHello "pmapp/api/hello"
+
+	"google.golang.org/grpc"
 )
 
-func main() {
-    log.Println("start service")
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+const (
+	port = 9001
+)
+
+type HelloServer struct {
+	pbHello.UnimplementedHelloServer
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    log.Println("hello request")
-    fmt.Fprintf(w, "Hello!\n")
+func NewHelloServer() *HelloServer {
+	return &HelloServer{}
+}
+
+func (helloServ *HelloServer) Install(req *pbHello.InstallRequest, stream pbHello.Hello_InstallServer) error {
+	var err error
+	log.Println("install request:", req)
+
+	var wg sync.WaitGroup
+	doneChan := make(chan bool)
+
+	aliveFunc := func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-doneChan:
+				log.Println("cancel alive func")
+				return
+			default:
+				time.Sleep(1 * time.Second)
+			}
+			log.Println("send alive response")
+			intermRes := pbHello.InstallResult{
+				Done: false,
+			}
+			err := stream.Send(&intermRes)
+			if err != nil {
+				log.Println("send alive error")
+				return
+			}
+		}
+	}
+	wg.Add(1)
+	go aliveFunc()
+
+	time.Sleep(120 * time.Second)
+	log.Println("installation is finished!")
+
+	doneChan <- true
+	wg.Wait()
+
+	doneRes := pbHello.InstallResult{
+		Done: true,
+	}
+	err = stream.Send(&doneRes)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func main() {
+	var err error
+
+	addresses, err := net.InterfaceAddrs()
+
+	if err == nil {
+		for _, addr := range addresses {
+			fmt.Println("service ip addess:", addr)
+
+		}
+	}
+	address := fmt.Sprintf(":%d", port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServ := grpc.NewServer()
+	pbHello.RegisterHelloServer(grpcServ, NewHelloServer())
+
+	log.Printf("server listening at %d", port)
+	err = grpcServ.Serve(listener)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
